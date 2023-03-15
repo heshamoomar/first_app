ed7748938c8c4bd7714a299159389006f0b9f499
Hesham Omar <hesham.omar1122@gmail.com>
2023-03-15 18:49:49 +0200

    my changes

---
+++ lib/question.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+
+  final String questionText;
+
+  //  constructor
+  const Question(this.questionText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText, style: ,);
+  }
+}
